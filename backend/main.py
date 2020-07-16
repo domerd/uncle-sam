484e9bc416d909f081d0c1f0da868ac50ea31107
Omer Doron <omer0411@gmail.com>
2020-07-16 12:01:15 +0300

    Add basic flask jwt backend

---
+++ backend/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+
+
+@app.route('/')
+def alive():
+    return 'I am alive!'
+
+
+@app.route('/protected')
+@jwt_required
+def protected():
+    return '%s' % get_jwt_identity()
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if username != 'test' or password != 'test':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token), 200
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
