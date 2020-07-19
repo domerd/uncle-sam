c159bea79fabfd27def60e2625a9e631da5f340c
Omer Doron <omer0411@gmail.com>
2020-07-19 16:17:15 +0300

    Add config modal

---
+++ src/ConfigModal.js
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+    Button, Form, Modal, Select,
+} from 'antd';
+import { SettingOutlined } from '@ant-design/icons';
+import _ from 'lodash';
+import { UserStoreContext } from './UserStore';
+import { CountryStoreContext } from './CountryStore';
+
+const ConfigModal = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const { defaultCountry, setDefaultCountry } = useContext(UserStoreContext);
+    const { countries } = useContext(CountryStoreContext);
+    const [form] = Form.useForm();
+
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+
+    useEffect(() => {
+        if (!defaultCountry) {
+            setModalOpen(true);
+        }
+    }, []);
+
+    const saveChanges = () => {
+        setDefaultCountry(form.getFieldValue('country'));
+        setModalOpen(false);
+    };
+
+    return (
+        <>
+            <Button icon={<SettingOutlined />} onClick={openModal} />
+            {modalOpen && (
+                <Modal
+                    visible
+                    closable={false}
+                    className="config-modal"
+                    title="User Configuration"
+                    onOk={saveChanges}
+                    onCancel={closeModal}
+                >
+                    <Form form={form} initialValues={{country: defaultCountry}}>
+                        <Form.Item name="country" label="Default Country">
+                            <Select>
+                                {!_.isEmpty(countries)
+                && _.map(countries, (country) => (
+                    <Select.Option value={country.country_id}>
+                        {country.name}
+                    </Select.Option>
+                ))}
+                            </Select>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+            )}
+        </>
+    );
+};
+
+export default ConfigModal;
--- src/HomePage.js
+++ src/HomePage.js
@@ -2,21 +2,19 @@
  * Created by omerdoron on 16/07/2020.
  */
 
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import { getFarmers } from './actions';
-import { RoadsStoreContext } from './RoadsStore';
+import ConfigModal from './ConfigModal';
 
 const HomePage = () => {
-    const { roads } = useContext(RoadsStoreContext);
-    console.log('In homepage', roads);
-
     useEffect(() => {
         getFarmers();
     }, []);
 
     return (
         <div>
+            <ConfigModal />
             <img src={logo} className="App-logo" alt="logo" />
           This is the home page
         </div>
