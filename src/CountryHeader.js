8419da8a7d7f8febe45924584479c43611eff1e1
danielko2000 <danielko2000@gmail.com>
2020-07-21 11:34:16 +0300

    edit country header

--- src/CountryHeader.js
+++ src/CountryHeader.js
@@ -15,8 +15,16 @@ const CountryHeader = () => {
 
     return (
         <div id="country-container">
+
             <Tag style={{ fontSize: 'large', padding: 10 }} color="white">
-                <h1>{country}</h1>
+                <h2>
+                    Deliveries in:
+                    {' '}
+                </h2>
+
+                <h1>
+                    {country}
+                </h1>
             </Tag>
         </div>
     );
