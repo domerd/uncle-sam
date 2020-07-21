3918b5130a1fbc65391c7543777dea5cb276fcf4
Omer Doron <omer0411@gmail.com>
2020-07-21 12:34:17 +0300

    Get map layout from backend

--- src/actions.js
+++ src/actions.js
@@ -7,3 +7,5 @@ import axios from 'axios';
 export const getFarmers = () => axios.get('/api/farmers');
 
 export const getCountries = () => axios.get('/api/countries');
+
+export const getMap = () => axios.get('/api/map');
